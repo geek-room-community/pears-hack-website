960ff0ee7b21f7f4b98b0ab3dc4f2b4419589f40
ManasKush <manaskush311@gmail.com>
2025-01-18 22:15:29 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import ScrollContent from './Components/scrollcontent';
 import PieChart from './Components/circulardiv';
 import './App.css';
 
+
 function App() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <>
       <div className="scroll-content-container">
-        <AboutUs /> {/* Replace with your content pages */}
         <ScrollContent
           totalIndexes={10}
           onIndexChange={(index) => setActiveIndex(index)} // Update active index
