5540d150fa494161ab772324887d0251f4c9ff07
Nibedan <nibedanpati@gmail.com>
2025-01-19 02:32:37 +0530

    .

--- src/ContentPages/Prize.jsx
+++ src/ContentPages/Prize.jsx
@@ -180,7 +180,7 @@ const Prize = () => {
           }
         `}
       </style>
-      <div style={containerStyle} className="container-style">
+      <div style={containerStyle} className="container-style"> 
         <div style={gridStyle} className="prize-grid">
           {/* AirPods Card */}
           <div style={cardStyle} className="prize-card">
