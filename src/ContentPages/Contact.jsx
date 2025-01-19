80d786c0208e3f0fe6402b40bf361ccd97a97334
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-01-19 19:46:05 +0530

    Correct linkedin svg

--- src/ContentPages/Contact.jsx
+++ src/ContentPages/Contact.jsx
@@ -1,19 +1,22 @@
 // ContactSection.jsx
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
 import Email from "../SVGs/email.svg";
 import Web from "../SVGs/website.svg";
 import Instagram from "../SVGs/instagram.svg";
-import LinkedIn from "../SVGs/linkedin.svg";
+import LinkedIn from "../SVGs/LinkedIn.svg";
 
 const Contact = () => {
   return (
     <div className="contact-container">
-      <h2 className="contact-heading"><span className="highlight">GOT</span> A QUERY?</h2>
-      <p className="contact-intro">We're here to assist you! Reach out to us through:</p>
-      
+      <h2 className="contact-heading">
+        <span className="highlight">GOT</span> A QUERY?
+      </h2>
+      <p className="contact-intro">
+        We're here to assist you! Reach out to us through:
+      </p>
+
       <div className="contact-info">
-        
         <a href="mailto:community@geekroom.in" className="contact-item">
           <img className="contact-icon" src={Email} alt="email icon" />
           <span className="contact-text">community@geekroom.in</span>
@@ -21,20 +24,35 @@ const Contact = () => {
       </div>
 
       <p className="social-intro">Also follow us on our social media handles</p>
-      
+
       <div className="social-links">
-        <a href="https://www.geekroom.in/" target="_blank" rel="noopener noreferrer" className="social-item">
-          <img className="social-icon" src= { Web } alt="website icon" />
+        <a
+          href="https://www.geekroom.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-item"
+        >
+          <img className="social-icon" src={Web} alt="website icon" />
           <span className="social-text">www.geekroom.in</span>
         </a>
-        
-        <a href="https://www.instagram.com/geek_suite/" target="_blank" rel="noopener noreferrer" className="social-item">
-          <img className="social-icon" src= { Instagram } alt="instagram icon" />
+
+        <a
+          href="https://www.instagram.com/geek_suite/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-item"
+        >
+          <img className="social-icon" src={Instagram} alt="instagram icon" />
           <span className="social-text">geek_suite</span>
         </a>
-        
-        <a href="https://www.linkedin.com/company/geek-suite/" target="_blank" rel="noopener noreferrer" className="social-item">
-          <img className="social-icon" src= { LinkedIn } alt="linkedin icon" />
+
+        <a
+          href="https://www.linkedin.com/company/geek-suite/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-item"
+        >
+          <img className="social-icon" src={LinkedIn} alt="linkedin icon" />
           <span className="social-text">Geek Suite</span>
         </a>
       </div>
